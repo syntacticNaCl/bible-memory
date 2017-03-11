39374cef4fa46084c83ae750be2b0d6b5bea9951
Syntactual <elanjoshrand@gmail.com>
2017-03-11 14:26:01 -0600

    simplify for testng

--- handlers.js
+++ handlers.js
@@ -1,16 +1,1 @@
 
-const handle = {
-    ReadVerse : function(){
-        instance.get('/John%203%3A16')
-        .then(function(res) {
-            var scriptureText = res.data.text;
-            console.log(scriptureText);
-            this.emit(':tell', scriptureText.text);
-        })
-        .catch( function(error){
-            console.log(error);
-        });
-    }
-}
-
-module.exports = handle;
--- index.js
+++ index.js
@@ -2,11 +2,28 @@
 
 const axios = require('axios');
 const Alexa = require('alexa-sdk');
-const alexaHandlers = require('/handlers');
+//const alexaHandlers = require('/handlers');
 
 const instance = axios.create({
     baseURL: 'https://c4tk.contentplatform.prod.lifeway.com/scripture/'
 });
+
+const handlers = {
+    ReadVerse : function(){
+        instance.get('/John%203%3A16')
+        .then(function(res) {
+            var scriptureText = res.data.text;
+            console.log(scriptureText);
+            this.emit(':tell', scriptureText.text);
+        })
+        .catch( function(error){
+            console.log(error);
+        });
+    },
+    SayVerse : function(Book, Chapter, Verse) {
+
+    }
+}
 
 
 exports.handler = function (event, context) {
@@ -16,6 +33,6 @@ exports.handler = function (event, context) {
     // alexa.APP_ID = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
     // alexa.resources = languageStrings; 
-    alexa.registerHandlers(alexaHandlers.handle);
+    alexa.registerHandlers(handlers);
     alexa.execute();
 };
