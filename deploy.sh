78ee3d1e14a8c8d27a38b2269aa9559addc95365
Ryan Means <ryan.means@lifeway.com>
2017-03-11 13:51:45 -0600

    deploy region

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ npm install \
 &&
 zip -R bibLambda.zip handlers.js index.js package.json node_modules/* \
 &&
-aws cloudformation package --template bible-mem-sam.yaml --s3-prefix bible-mem-lambda --s3-bucket c4tk-alexa --output-template-file bible-mem-sam-packaged.yaml \
+aws cloudformation package --template bible-mem-sam.yaml --s3-prefix bible-mem-lambda --s3-bucket c4tk-alexa --output-template-file bible-mem-sam-packaged.yaml -region=us-east-1 \
 &&
-aws cloudformation deploy --template-file bible-mem-sam-packaged.yaml --stack-name sam-bible-mem
+aws cloudformation deploy --template-file bible-mem-sam-packaged.yaml --stack-name sam-bible-mem --region=us-east-1
