8775388f1e6a97f8a22d84468aa0ce9c90736bbe
Ryan Means <ryan.means@lifeway.com>
2017-03-11 13:27:56 -0600

    auto deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ npm install \
 &&
 zip bibLambda.zip handlers.js index.js package.json node_modules/* \
 &&
-aws cloudformation package --template bible-mem-sam.yaml --s3-prefix bible-mem-lambda --s3-bucket c4tk-alexa --output-template-file bible-mem-sam-packaged.yaml
-# && \
-# aws cloudformation deploy --template-file bible-mem-sam-packaged.yaml --stack-name sam-bible-mem
+aws cloudformation package --template bible-mem-sam.yaml --s3-prefix bible-mem-lambda --s3-bucket c4tk-alexa --output-template-file bible-mem-sam-packaged.yaml \
+&&
+aws cloudformation deploy --template-file bible-mem-sam-packaged.yaml --stack-name sam-bible-mem
