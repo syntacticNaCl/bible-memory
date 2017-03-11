deb5213d50131b709f35956593d722de99836961
syntacticNaCl <garrettrappaport@gmail.com>
2017-03-11 13:10:09 -0500

    remove duplication

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ const instance = axios.create({
 });
 
 
-
-exports.handler = (event, context) => {
-    const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(HANDLERS.handlers);
-    alexa.execute();
-};
-
-
-
-
-
 export.handlers = function (event, context) {
 
     let alexa = Alexa.handler(event, context);
