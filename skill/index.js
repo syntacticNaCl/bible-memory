a4c33259ad4bfa5b162333c73aa3803da3e3aea7
Syntactual <elanjoshrand@gmail.com>
2017-03-11 15:09:17 -0600

    ks kdkkk

--- skill/index.js
+++ skill/index.js
@@ -9,24 +9,30 @@ const instance = axios.create({
     baseURL: 'https://c4tk.contentplatform.prod.lifeway.com/scripture/'
 });
 
+
+
 const handlers = {
     ReadVerse : function(){
-
         var vm = this;
         instance.get('/John%203%3A16')
         .then(function(res) {
             var scriptureText = res.data.text;
             console.log(scriptureText);
-
             vm.emit(':tell', scriptureText);
-
         })
         .catch( function(error){
             console.log(error);
         });
     },
     SayVerse : function(Book, Chapter, Verse) {
-
+        let path = encodeURIComponent(`${Book} ${Chapter}:${Verse}`);
+        instance.get(`/${path}`)
+        .then(function(res){
+            console.log(res);
+        })
+        .catch(function(err){
+            console.log(err);
+        });
     },
     Unhandled : function() {
         this.emit(':tell', "Sorry for you I have not this intent");
