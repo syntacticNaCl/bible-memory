b70ab0589fc475efd3ad6ae8368ef8597781223b
Syntactual <elanjoshrand@gmail.com>
2017-03-11 11:31:03 -0600

    added alexa skill tell

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 'use strict'
 
 const axios = require('axios');
+const Alexa = require('alexa-sdk');
 
 const instance = axios.create({
     baseURL: 'https://c4tk.contentplatform.prod.lifeway.com/scripture/'
 });
 
 
-instance.get('/John%203%3A16').then(function(res) {
-    var scriptureText = res.data.text;
-    console.log(scriptureText);
-}).catch( function(error){
-    console.log(error);
-});
+instance.get('/John%203%3A16')
+        .then(function(res) {
+            var scriptureText = res.data.text;
+            console.log(scriptureText);
+            this.emit(':tell', scriptureText.text);
+        })
+        .catch( function(error){
+            console.log(error);
+        });
 
